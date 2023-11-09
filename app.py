7cbbfcf018da23f77d2383f80e86be9ea7fed1c8
Vikas Phulriya <88418134+vikasphulariya@users.noreply.github.com>
2023-11-09 10:56:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import hashlib
+import requests
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/verify_payment', methods=['POST'])
+def verify_payment():
+    key = request.form.get('key')
+    txnid = request.form.get('txnid')
+    salt = request.form.get('salt')
+
+    data = f"{key}|verify_payment|{txnid}|{salt}"
+    hash = hashlib.sha512(data.encode()).hexdigest()
+
+    url = "https://test.payu.in/merchant/postservice?form=2"
+    payload = f"key={key}&command=verify_payment&var1={txnid}&hash={hash}"
+
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    response = requests.post(url, data=payload, headers=headers)
+    return jsonify(response.text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
